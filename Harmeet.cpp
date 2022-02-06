66e90a397aaef4e9074000898e236fc126132b2e
NitiKaur <nitikaur102@gmail.com>
2022-02-06 21:34:17 +0530

    my first commit

---
+++ Harmeet.cpp
@@ -0,0 +1,18 @@
+ #include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int x=4,y=12;
+if(x=5||y>50)
+{
+  cout<<"Dean of student affairs."<<endl;
+
+}
+else{
+  cout<<"dosa";
+}
+
+
+  return 0;
+
+}
